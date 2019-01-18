d05d4507ea810eb9d9d2a7a2a4ba8169e722f7f7
quatoo <filipequatoo@yahoo.com.br>
2019-01-18 17:44:05 -0200

    Add files via upload

---
+++ webpack.common.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/main.ts'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'LiturgyReader',
+            hash: true,
+            minify: {
+                html5: true, 
+                collapseWhitespace: true, 
+                removeComments: true
+            },
+            filename: 'index.html',
+            template: __dirname + '/src/index.html'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            { 
+                test: /\.css$/, 
+                loader: 'style-loader!css-loader' 
+            },
+            { 
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff' 
+            },
+            { 
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+            },
+            { 
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'file-loader' 
+            },
+            { 
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader?limit=10000&mimetype=image/svg+xml' 
+            }
+        ]
+    }
+}
